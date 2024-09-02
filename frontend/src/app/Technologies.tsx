214be023075e5c8eb8697151f55e7d90f1874c04
zRealReax <rex.max11995@gmail.com>
2024-09-02 09:16:01 +0200

    Creating the Sections

---
+++ frontend/src/app/Technologies.tsx
@@ -0,0 +1,3 @@
+export default function Technologies() {
+  return <main>Test</main>;
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,21 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import AboutMe from "./AboutMe";
+import Technologies from "./Technologies";
+import Projects from "./Projects";
+import Demo from "./Demo";
+import Footer from "./Footer";
+
 export default function Home() {
-  return <main>Welcome</main>;
+  return (
+    <main className="w-screen bg-neutral-100 min-h-screen">
+      <Navbar />
+      <Hero />
+      <AboutMe />
+      <Technologies />
+      <Projects />
+      <Demo />
+      <Footer />
+    </main>
+  );
 }
